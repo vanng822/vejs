0adcff993df0447d82e5a0cdaffc5bc323408961
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-06-20 11:17:51 +0200

    Fix for online comment in javascript when running eatspace

--- tests/test_parse.js
+++ tests/test_parse.js
@@ -56,6 +56,14 @@ vows.describe('Test suite for parsing template').addBatch({
 		assert.equal(vejs.parse('{{- &obj.func1() }}'), expected);	
 		vejs.open = '<%';
         vejs.close = '%>';
+	},
+	parseWithJsOnlineComment: function() {
+		var expected = "var buf = [];";
+		expected += "\nwith (locals) {";
+		expected += "\n  buf.push('<div>first div</div><script>/* comment 1 */</script><p>p</p><script type=\"text/javascript\">var t = 1; /* comment 2 */ var nvar=2;</script><div>', (__stack.lineno=3,  funcs.obj.func1() ), '</div>');";
+		expected += "\n}\n";
+		expected += "return buf.join('');";
+		var output = vejs.parse('<div>first div</div><script>//comment 1</script>\n<p>p</p><script type="text/javascript">var t = 1; //comment 2\n var nvar=2;</script><div><%- &obj.func1() %></div>');
+		assert.equal(output, expected);	
 	}
-	
 }).export(module);
